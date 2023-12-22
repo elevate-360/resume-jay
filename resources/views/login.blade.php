d4c5c44f0078300eca97c7ee2cfa3bb4ef8b1be5
SF-dev1 <stylefeathers.dev.1@gmail.com>
2023-12-22 15:36:33 +0530

    title bug fixed

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Elevate360 | Money Log in</title>
+    <title>Contact Dashboard | Log in</title>
 
     <!-- Favicons -->
     <link href="dist/img/favicon.png" rel="icon">
